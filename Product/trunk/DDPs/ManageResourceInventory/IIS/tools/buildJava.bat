135c0cff005d9e20c4b8c12c17edca9a59387e7a
jeromem <jeromem@77982bfb-fc12-0410-be40-88b8b568af2b>
2007-12-04 19:01:06 +0000

    IIS derived artifacts generation

---
+++ Product/trunk/DDPs/ManageResourceInventory/IIS/tools/buildJava.bat
@@ -0,0 +1,19 @@
+REM PC batch file (script) for generating the Java library of this DDP using XMLBeans
+
+set DDP_HOME=..
+set CODE_HOME=%DDP_HOME%\Java
+set SRC_HOME=%CODE_HOME%\src
+set CLASS_HOME=%CODE_HOME%\class
+
+set XMLBEANS_LIB=..\..\..\..\..\..\tools\xmlBeansLib
+set CP=%XMLBEANS_LIB%/xbean.jar;%XMLBEANS_LIB%\jsr173_1.0_api.jar;%XMLBEANS_LIB%\resolver.jar;%JAVA_HOME%\lib\tools.jar
+
+REM Generate using XMLBeans
+java -Xmx256m -classpath "%cp%" org.apache.xmlbeans.impl.tool.SchemaCompiler -mx 800m  -d %CLASS_HOME% -src %SRC_HOME% -out %DDP_HOME%\derived\New_ManageResourceInventory.jar -javasource 1.5 -debug ..\xsd
+
+REM Package into library
+cd %SRC_HOME%
+jar -cvf ..\..\derived\New_ManageResourceInventoryJava.zip .
+cd ..\..\tools
+
+pause
